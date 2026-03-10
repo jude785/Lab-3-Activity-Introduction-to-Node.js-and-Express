862ef4ed67730309b6a6e0c1e33d71d5ff2b9b17
jude785 <velosojudekhalil@gmail.com>
2026-03-10 21:00:13 +0800

    final product

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const newItem = req.body.item;
+  items.push(newItem);
+  res.join(items)
 });
 
 app.listen(port, () => {  
